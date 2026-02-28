582c58ce794840b7c90626a9a0dab0b0d29c2405
Adrian Anicete <anicete.ian@gmail.com>
2026-02-28 17:34:07 +0800

    express-1

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-const express = require('express');
-const PORT = 8000
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js'
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/errorHandler.js';
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+// setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// logger middleware
+app.use(logger)
+
+
+
+app.use('/api/posts/', posts)
+
+// errorHandler
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
